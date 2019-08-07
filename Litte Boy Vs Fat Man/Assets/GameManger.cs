2050d25f59f147c6d7b40f573aa4f13fca527e4e
Anuj Shrestha <anuj777@hotmail.com>
2019-08-07 09:55:22 +0545

    LB-1 Add repeating background

---
+++ Litte Boy Vs Fat Man/Assets/GameManger.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManger : MonoBehaviour
+{
+    public GameObject bg;
+    public int bgPool = 3;
+    List<GameObject> bgs;
+
+    void Start()
+    {
+        bgs = new List<GameObject>();
+
+        for (int i = 0; i < bgPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(bg);
+            obj.SetActive(false);
+            bgs.Add(obj);
+
+        }
+
+        spawnBg();
+    }
+
+    void spawnBg()
+    {
+        for (int i = 0; i < bgs.Count; i++)
+        {
+            //RectTransform rt = (RectTransform) bgs[i].transform;
+            //float imgHeight = rt.rect.height;
+            float imgHeight = bgs[i].GetComponent<SpriteRenderer>().sprite.bounds.size.x;  // this is gonna be our height
+            bgs[i].transform.position = new Vector3(transform.position.x, imgHeight * i, transform.position.z);
+            bgs[i].SetActive(true);
+        }
+    }
+
+
+}
---
+++ Litte Boy Vs Fat Man/Assets/MoveBackground.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MoveBackground : MonoBehaviour
+{
+    public float BGMoveSpeed = 1f;
+    SpriteRenderer m_SpriteRenderer;
+    float imgHeight;
+    private void Start()
+    {
+        imgHeight = GetComponent<SpriteRenderer>().sprite.bounds.size.x;  // this is gonna be our height
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(-BGMoveSpeed * Time.deltaTime, 0, 0);
+        if (transform.position.y <= -imgHeight)
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position = new Vector3 (transform.position.x, imgHeight * 2, transform.position.z);
+    }
+
+
+}
